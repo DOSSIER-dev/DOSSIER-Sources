37bb62b6bb9a125be7495e02342e9950ac1760fd
Matthias Frey <info@frey-it.com>
2020-02-11 16:37:04 +0100

    fix: tolerate missing database

--- db-import.sh
+++ db-import.sh
@@ -16,11 +16,9 @@ source .env
 if [[ "$@" == "NO_CLI" ]]
 then
   export COMPOSE_INTERACTIVE_NO_CLI=1
-  docker-compose exec db bash -c "mysqladmin -u ${SOURCES_MYSQL_USER} --password=${SOURCES_MYSQL_PASSWORD} drop ${SOURCES_MYSQL_DATABASE} -f"
-else
-  docker-compose exec db bash -c "mysqladmin -u ${SOURCES_MYSQL_USER} --password=${SOURCES_MYSQL_PASSWORD} drop ${SOURCES_MYSQL_DATABASE}"
 fi
 
+docker-compose exec db bash -c "mysql -u ${SOURCES_MYSQL_USER} --password=${SOURCES_MYSQL_PASSWORD} -e 'drop database if exists ${SOURCES_MYSQL_DATABASE}'"
 docker-compose exec db bash -c "mysqladmin -u ${SOURCES_MYSQL_USER} --password=${SOURCES_MYSQL_PASSWORD} create ${SOURCES_MYSQL_DATABASE} -f"
 docker-compose exec db bash -c "mysql sources -u ${SOURCES_MYSQL_USER} --password=${SOURCES_MYSQL_PASSWORD} < /var/dump/testdb.sql"
 docker-compose exec webapp /code/manage.py search_index --rebuild -f
